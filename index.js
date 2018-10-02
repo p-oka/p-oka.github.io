842366f554122e2093682cd2416d05997b3e9e0b
Kohei Kataoka <kata0hka@gmail.com>
2018-10-02 17:06:44 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  window.fcWidget.init({
+    token: "3f7b8ceb-c6e9-4240-90e7-ef27f947d7c5",
+    host: "https://wchat.freshchat.com"
+  });
+});
